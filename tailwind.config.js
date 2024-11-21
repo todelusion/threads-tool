047a30f28af835cea39dddeec4a8e050c3c9b853
todelusion <todelusion@gmail.com>
2024-11-21 18:46:57 +0800

    Fixed performence issue due to Google font import

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    fontFamily: {
-      montserrat: ["Montserrat", "sans-serif"],
-      sans: ["Noto Sans TC", "sans-serif"],
-    },
   },
   plugins: [],
 };
